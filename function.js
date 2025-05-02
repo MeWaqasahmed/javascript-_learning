3785c6786fd1462762d78b4169318fe4ea90a39a
MeWaqasahmed <mwahmed009@gmail.com>
2025-05-03 01:42:22 +0500

    function to find the repetition of a number in an array

---
+++ function.js
@@ -0,0 +1,11 @@
+const array = [7, 6, 7, 8, 7, 6, 5, 7, 8];
+function target(array , number) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++){
+    if (number == array[i]) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(target(array, 8));
