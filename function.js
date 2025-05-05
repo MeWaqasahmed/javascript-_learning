c0648a708724416cf44479f30fa19a9991314783
MeWaqasahmed <mwahmed009@gmail.com>
2025-05-05 23:06:19 +0500

    working on lexical scope concept

--- function.js
+++ function.js
@@ -1,11 +1,20 @@
-const array = [7, 6, 7, 8, 7, 6, 5, 7, 8];
-function target(array , number) {
-  let count = 0;
-  for (let i = 0; i < array.length; i++){
-    if (number == array[i]) {
-      count++;
-    }
-  }
-  return count;
+// const array = [7, 6, 7, 8, 7, 6, 5, 7, 8];
+// function target(array , number) {
+//   let count = 0;
+//   for (let i = 0; i < array.length; i++){
+//     if (number == array[i]) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(target(array, 8));
+
+// --------------Arrow function---------------
+
+let waq = "waqas";
+const func = () => {
+  // return waq;
+  console.log(waq); 
 }
-console.log(target(array, 8));
+ func();
